bb78fb3237d34fa6b9d6ccf7b9b50e882c3b8f81
johnrraymond <johnrraymond@yahoo.com>
2023-03-12 04:51:36 +0000

    added in a model for where the marketplace could go.

--- bakerydemo/art/embed.js
+++ bakerydemo/art/embed.js
@@ -5,12 +5,31 @@ const e = React.createElement;
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+
+    let liked = getLibraryProperty("liked");
+    console.log("liked: ", liked);
+    if(null == liked) {
+       liked = false; // Ask api...
+    } 
+    if(liked == "true") {
+       liked = true;
+    } else {
+       liked = false;
+    }
+    console.log("final state: ", liked);
+    this.state = { liked: liked }; 
   }
 
   render() {
+    
     if (this.state.liked) {
-      return 'You liked comment number ' + this.props.commentID;
+      const bookId = getLibraryProperty("bookid");
+      const url = new URL(window.location.href);
+      url.searchParams.set("liked", "true");
+      url.searchParams.set("bookid", bookId);
+      window.history.pushState({}, "", url.toString());
+	
+      return 'You liked book ' + getLibraryProperty("bookid"); // + JSON.stringify(this.props); // this.props.commentID;
     }
 
     return e(
@@ -25,20 +44,61 @@ class LikeButton extends React.Component {
 document.querySelectorAll('.like_button_container')
   .forEach(domContainer => {
     // Read the comment ID from a data-* attribute.
-    const commentID = parseInt(domContainer.dataset.commentid, 10);
+    const commentID = getLibraryProperty("bookid"); //"parseInt(domContainer.dataset.commentid, 10);"
     const root = ReactDOM.createRoot(domContainer);
     root.render(
       e(LikeButton, { commentID: commentID })
     );
   });
 
-function doit1 (myspan) {
-        console.log("In do it");
 
-        const domContainer = document.querySelector('#like_button_container_div');
-        const root = ReactDOM.createRoot(domContainer);
-        root.render(e(LikeButton));
 
-        //const root = ReactDOM.createRoot(domContainer);
-        //root.render(e(LikeButton));
+
+console.log("In do it");
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+//btn.onclick = function() {
+	//modal.style.display = "block";
+//}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	modal.style.display = "none";
 }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+
+function doit1 (myspan) {
+	console.log(myspan);
+
+	modal.style.display = "block";
+}
+
+const domContainer = document.querySelector('#like_button_container_div');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
+
+function getLibraryProperty(propKeyName) {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  const value = urlParams.get(propKeyName);
+
+  console.log(propKeyName + ": ", value);
+  return value;
+}
+
+// All react renders take place at some point after this is executed.
+
--- bakerydemo/art/views.py
+++ bakerydemo/art/views.py
@@ -1446,7 +1446,46 @@ def testalice(request):
     c.content=u"""
 <div>
 
-  <div id="like_button_container_div"></div>
+<!-- Trigger/Open The Modal -->
+<!--button id="myBtn">Open Modal</button-->
+
+<!-- The Modal -->
+<div id="myModal" class="modal" style="
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+">
+
+  <!-- Modal content -->
+  <div class="modal-content" style="
+   background-color: #fefefe;
+   margin: auto;
+   padding: 20px;
+   border: 1px solid #888;
+   width: 80%; 
+">
+    <span class="close" style="
+     color: #aaaaaa;
+     float: right;
+     font-size: 28px;
+     font-weight: bold;
+">&times;</span>
+    <p>Inset iframe with the marketplace here.</p>
+  </div>
+
+</div>
+
+
+  <div id="modal_container_div"></div>
+  <div id="like_button_container_div" ></div>
   <h1 onclick="doit1(this)" id="like_button_container">Chapter 1</h1><p>Down the Rabbit-Hole</p>
 
 </div>
